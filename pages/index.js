5de752c0ef929fc48df53466e3e9f1470a9a3302
mayragg <102768682+mayragg@users.noreply.github.com>
2023-01-31 13:13:45 -0800

    changing css for home and then aaddded about us page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>My Little Unicorn</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Care & Love?</h1>
+        <div className="navBar">
+          {' '}
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+
+            <li>
+              <a href="#gallery">Gallery</a>
+            </li>
+            <li>
+              <a Link href="/_about">About Us</a>
+            </li>
+          </ul>
+        </div>
+        <h1 className={styles.title} id="care">
+          Care & Love?{' '}
+        </h1>
 
         <p className="adopt"> Adopt </p>
 
@@ -44,11 +62,11 @@ export default function Home() {
         </div> */}
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a href="https://next.new" target="_blank" rel="noopener noreferrer">
           Created with&nbsp;<b>next.new</b>&nbsp;⚡️
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
